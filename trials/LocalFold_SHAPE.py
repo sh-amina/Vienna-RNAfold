734dfb4fed9a616e1df56dadc2861bbc96ff6399
sh-amina <nazilaamina@gmail.com>
2022-03-25 08:00:24 +0100

    Last week's update

--- trials/LocalFold_SHAPE.py
+++ trials/LocalFold_SHAPE.py
@@ -1,54 +1,52 @@
 import RNA
 import random
 
-#convert .map file to CSV
-# with open("t-RNA/Mg_1M7_1_29903.map") as fh:
-#     lines = fh.readlines()
-# out = open('csv-2.csv','w')
-# for line in lines:
-# 	line = line.split()
-# 	out.write(line[0])
-# 	out.write(',')
-# 	if line[1] == 0:
-# 		out.write('nan')
-# 	else:
-# 		out.write(line[1])
-# 	out.write("\n")
-# 	print(line[0],line[1],line[2])
-
-
-def getShapeDataFromFile(shape_file):
+def getShapeFromCSV(csv_file):
     retVec = []
     retVec.append(-999.0);  # data list is 1-based, so we add smth. at pos 0
-    count = 1
-    with open(shape_file, 'r') as f:
+    with open(csv_file, 'r') as f:
         lines = f.readlines()
     for line in lines:
-        line = line.split()
-        pos = int(line[0])
-        value = float(line[1])
-
-        if(pos==count):
-            retVec.append(value)
+        line = line.split(',')
+        if line[1] == 'nan\n':
+            retVec.append(-999.0)
         else:
-            for i in range(pos-count):
-                retVec.append(-999.0)
-            retVec.append(value)
-            count=pos
-        count += 1
+            retVec.append(float(line[1][:-1]))
+    return retVec
+# res = getShapeFromCSV('full1.csv')
+
+def get_SHAPE_average(file1,file2):
+    retVec = []
+    retVec.append(-999.0);  # data list is 1-based, so we add smth. at pos 0
+    with open(file1, 'r') as f:
+        lines1 = f.readlines()
+    with open(file2, 'r') as f:
+        lines2 = f.readlines()
+    for i in range(len(lines1)):
+        line1 = lines1[i].split(',')
+        if line1[1] == 'nan\n':
+            a = -999
+        else:
+            a = float(line1[1][:-1])
+
+        line2 = lines2[i].split(',')
+        if line2[1] == 'nan\n':
+            b = -999
+        else:
+            b = float(line2[1][:-1])
+        avr = (a+b)*0.5
+        retVec.append(avr)
+    # print(retVec)
     return retVec
 
+# res = get_SHAPE_average('full1.csv','full2.csv')
 
-# res = getShapeDataFromFile('t-RNA/Mg_1M7_1_29903.map')
-# print(res)
 # A positive slope m penalizes high reactivities in paired regions, 
 # while a negative intercept b results in a confirmatory `‘bonus’' 
 # free energy for correctly predicted base pairs
-
-#  m= 1.9
+#  m = 1.9
 #  b = -0.7
-
-def initialize_fold(sequence_file, output_file, size, shape_file = None, m=None, b=None, hc= None, sc = None):
+def initialize_fold(sequence_file, output_file, size, shape_file = None, shape_flag = None, m=None, b=None, hc= None, sc = None):
     with open(sequence_file) as fh:
         string = fh.readlines()
     sequence = ""
@@ -63,8 +61,28 @@ def initialize_fold(sequence_file, output_file, size, shape_file = None, m=None,
     fc = RNA.fold_compound(sequence,md,RNA.OPTION_WINDOW)
 	# Check for shape data
     if shape_file is not None:
-        shape = getShapeDataFromFile(shape_file)
-        fc.sc_add_SHAPE_deigan(shape,m,b)
+        if len(shape_file) == 2:
+            if shape_flag:
+                file1, file2 = shape_file
+                shape = get_SHAPE_average(file1,file2)
+                fc.sc_add_SHAPE_deigan(shape,m,b)
+            else:
+                fc2 = fc
+                shape1 = getShapeFromCSV(shape_file[0])
+                shape2 = getShapeFromCSV(shape_file[1])
+                fc.sc_add_SHAPE_deigan(shape1,m,b)
+                fc2.sc_add_SHAPE_deigan(shape2,m,b)
+                file1, file2 = 'file1.fasta', 'file2.fasta'
+                with open(file1,"w") as fh:
+                    mfe = fc.mfe_window(fh)
+                with open(file2,"w") as fh:
+                    mfe = fc2.mfe_window(fh)
+                return(file1,file2,sequence)
+        else:
+            shape = getShapeFromCSV(shape_file)
+            # print(shape[:100])
+            fc.sc_add_SHAPE_deigan(shape,m,b)
+        
     # Check for constraints
     if hc is not None:
         # constrain some bases as unpaired and fold again
@@ -77,15 +95,33 @@ def initialize_fold(sequence_file, output_file, size, shape_file = None, m=None,
         fc.hc_add_bp(a,b) 
     with open(output_file,"w") as fh:
         mfe = fc.mfe_window(fh)
-    file = open(output_file, mode = 'r', encoding = 'utf-8-sig')
-    return(file,sequence)
+    # file = open(output_file, mode = 'r', encoding = 'utf-8-sig')
+    # f = open("myfile2.txt", "x")
+    # f.write(file.read())
+    # f.close()
+    return(output_file,sequence)
 
-# x = initialize_fold('t-RNA/shape-seq.fasta', 't-RNA/output-shape.fasta', 50, 't-RNA/shape-react.fasta', 1, -1)
-# print(x)
 
-def window_fold(input_file, output_file, size, shape_file = None, m=None, b=None, hc= None, sc = None):
+def window_fold(input_file, output_file, size, shape_file = None, shape_flag = None, m=None, b=None, hc= None, sc = None):
     # Create and open file
-    file, sequence = initialize_fold(input_file, output_file, size, shape_file, m, b)#the function returns (file,sequence)
+    if len(shape_file) == 2 and not shape_flag:
+        # file2, sequence = initialize_fold(input_file, output_file, size, shape_file[0], m, b)#the function returns (file,sequence)
+        # file1 = initialize_fold(input_file, output_file, size, shape_file[1], m, b)[0]
+        file1, file2, sequence = initialize_fold(input_file, output_file, size, shape_file, shape_flag, m, b)#the function returns (file1,file2,sequence)
+        file2 = open(file2, "r")
+        data2 = file2.read()
+        file2.close()
+        file1 = open(file1, "r")
+        data1 = file1.read()
+        file1.close()
+        file = open('mix.fasta', "a")
+        file.write(data2)
+        file.write(data1)
+        file.close()
+        file = open('mix.fasta', mode = 'r', encoding = 'utf-8-sig')
+    else:
+        file, sequence = initialize_fold(input_file, output_file, size, shape_file, shape_flag, m, b)#the function returns (file,sequence)
+        file = open(file, mode = 'r', encoding = 'utf-8-sig')
     # Parse the t-RNA Sequence
     lines = file.readlines()
     seq = []
@@ -105,14 +141,12 @@ def window_fold(input_file, output_file, size, shape_file = None, m=None, b=None
     # Reformat data into useful lists
     intervals = []
     for window in seq:
-        (left,right,energy) = (int(window[2]),'L'), (int(window[2])+len(window[0]),'R'), float(window[1])
-        intervals.append((left,right,energy))   
+        (left,right,energy,string) = (int(window[2]),'L'), (int(window[2])+len(window[0]),'R'), float(window[1]), window[0]
+        intervals.append((left,right,energy,string))   
     intervals.reverse()
     I = []
     for i in range(len(intervals)):
-        # a c = location start/end
-        # b d = Left or Right 
-        # i = index of tuples from 'Interval'
+        # a,c = location start/end     b,d = Left or Right   i = index of tuples from 'Interval'
         a, b = intervals[i][0]
         c, d = intervals[i][1]
         I.append((a,b,i))
@@ -123,6 +157,7 @@ def window_fold(input_file, output_file, size, shape_file = None, m=None, b=None
     temp_min = -1
     V = [ (j[2],[j]) for j in intervals]
     v = [ j[2] for j in intervals]
+
     for i in range(len(I)):
         # Left of interval j, V[j] = v[j] + minimum
         if I[i][1] == 'L':
@@ -139,37 +174,31 @@ def window_fold(input_file, output_file, size, shape_file = None, m=None, b=None
             if V[j][0] <= minimum:
                 minimum = V[j][0]
                 temp_min = j  
+    # print('V: \n',V,'\n')
     result = []
-    for p in V[temp_min][1]:
-        left, right, energy = p[0][0], p[1][0], p[2]
-        result.append(([left,right], energy))
-    return(result,sequence) 
-
-
-def local_fold_list(input_file, output_file, size, shape_file = None, m=None, b=None, hc= None, sc = None):
-    # Create and open file
-    file = initialize_fold(input_file, output_file, size, shape_file, m, b)[0] #the function returns (file,sequence)
-    # Parse the t-RNA Sequence
-    lines = file.readlines()
     d = {}
-    for line in lines:
-        window = line.split()
-        #window:  ['.(((((.........))))).', '(', '-0.10)', '50']
-        # dictionary with {50 : '.(((((.........))))).' , etc.}
-        if window != []:
-            d[int(window[-1])] =  window[0]
-    return d
+    for p in V[temp_min][1]:
+        # print('p :',p,'\n')
+        left, right, energy = p[0][0], p[1][0], p[2]
+        d[left] = p[3]
+        result.append(([left,right], energy))
+    # print('res: \n',result, '\n')
+    # print('d: ', d, '\n')
+    return(result,sequence,d) 
 
+# res = window_fold('cov700.fasta', 'merge.lfold.chain', 200, 'cov1.csv' , 1.9, -0.7)
+# res = window_fold('cov700.fasta', 'merge.lfold.chain', 200, ('cov1.csv','cov2.csv') , 1.9, -0.7)
 
-def concatinate_local_fold(input_file, output_file, size, shape_file = None, m=None, b=None, hc= None, sc = None):
-    result, sequence = window_fold(input_file, output_file, size, shape_file, m, b) #the function returns (file,sequence)
+def concatinate_local_fold(input_file, output_file, size, shape_file = None, shape_flag = None, m=None, b=None, hc= None, sc = None):
+    result, sequence, D = window_fold(input_file, output_file, size, shape_file, shape_flag, m, b) #the function returns (file,sequence,d)
     # results = [([1, 37], -16.8), ([46, 74], -3.3), ([76, 122], -6.4), ([123, 164], -9.1), ([167, 199], -11.7)]
     seq_len = len(sequence)
-    D = local_fold_list(input_file, output_file, size, shape_file, m, b)
+    # D = local_fold_list(input_file, output_file, size, shape_file, m, b)
     # D = {13: '.((((.((((((((((.(.......).))))))........))))..)))).',
     #       2: '.((((..(((((((.....))))))))))).',
     #       1: '(((((.(((((((((.....))))))).))))))).' , etc.}
     sequence += '\n'
+    next = 1
     for i in range(len(result)):
         start = result[i][0][0]
         if i == 0:
@@ -192,16 +221,17 @@ def concatinate_local_fold(input_file, output_file, size, shape_file = None, m=N
     out.close()
     return(output)
 
+# res = concatinate_local_fold('cov700.fasta', 'merge.lfold.chain', 100, ('cov1.csv','cov2.csv') , 1.9, -0.7)
+
+# res = concatinate_local_fold('t-RNA/seq_isolat_PS.fasta', 'COVID-300.lfold.chain',300,'full2.csv',False, 1.8, -0.6)
+
+
+# res = concatinate_local_fold('fullcov.fasta', 'average-250.lfold.chain', 250, ('full1.csv','full2.csv') ,True, 1.9, -0.7)
+# res = concatinate_local_fold('fullcov.fasta', 'double-320.lfold.chain', 320, ('full1.csv','full2.csv') , 1.9, -0.7)
 # res = concatinate_local_fold('cov.fasta',200,'output')
 # res = concatinate_local_fold('t-RNA/shape-seq.fasta', 't-RNA/output-shape.fasta', 50, 't-RNA/shape-react.fasta', 1, -1)
-#  m= 1.9
-#  b = -0.7
 # res = concatinate_local_fold('t-RNA/sequence.fasta', 'browser files/sequence.lfold.chain', 20)
-res = concatinate_local_fold('t-RNA/seq_isolat_PS.fasta', 'browser files/COVID-300.lfold.chain',300,'t-RNA/Mg_1M7_1_29903.map', 1.9,-0.7)
+# res = concatinate_local_fold('t-RNA/seq_isolat_PS.fasta', 'browser files/COVID-300.lfold.chain',300,'t-RNA/Mg_1M7_1_29903.map', 1.9,-0.7)
 # res = concatinate_local_fold('t-RNA/seq_isolat_PS.fasta', 'browser files/COVID-300-noshape.lfold.chain',300)
 # res = concatinate_local_fold('t-RNA/sequence.fasta', 't-RNA/sequence-out.lfold.chain',20)
 # res = window_fold('cov.fasta',200,'output')
-# print(res)
- 
-
-# print('Heya')
